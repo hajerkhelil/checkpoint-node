bb03d3cdc4449943e819bed6f564f2d56274c30a
hajerkhelil <khelilhajer20@gmail.com>
2021-12-19 22:51:39 +0100

    node

---
+++ my-first-async-io.js
@@ -0,0 +1,7 @@
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const buffuerFile = fs.readFileSync(filePath, 'utf8', (err, data) => {
+	const numberLines = buffuerFile.toString().split('\n').length - 1;
+	console.log(numberLines);
+});
